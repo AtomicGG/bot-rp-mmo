ede52b1dfea63c59a534e9aed3882e215a41fb33
AtomicGG <43680840+AtomicGG@users.noreply.github.com>
2021-03-31 20:35:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,28 +41,28 @@ bot.on('message', message => {
             .setFooter("『XXXXX [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
             .setColor(3447003)
             .addField("Bienvenue sur XXXX !", "Il suffit d'écrire `=[Commande]` pour afficher les informations !\n\n" +
-                ":book: Concernant le contexte : `=Contexte`\n\n" +
-                ":notes: Concernant la création de personnage : `=Personnage`\n\n" +       
-                ":notes: Concernant les rolls : `=Roll`\n\n" +
-                ":notes: Concernant la santé : `=Santé`\n\n" +
-                ":notes: Concernant la faim : `=Faim`\n\n" +
-                ":notes: Concernant la soif : `=Soif`\n\n" +
-                ":notes: Concernant la fatigue : `=Fatigue`\n\n" +
-                ":notes: Concernant les combats : `=Combat`\n\n" +
-                ":notes: Concernant la fouille : `=Fouille`\n\n" +
-                ":notes: Concernant la récolte : `=Récolte`\n\n" +
-                ":notes: Concernant les différents métiers : `=Métiers`\n\n" +
-                ":notes: Concernant les taxes en ville : `=Taxes`\n\n" +
-                ":notes: Concernant la prison : `=Prison`\n\n" +
-                ":notes: Concernant les trajets : `=Trajets`\n\n" +
-                ":notes: Concernant les montures : `=Monture`\n\n" +
-                ":notes: Concernant le système de craft : `=Craft`\n\n" +
-                ":notes: Concernant la banque : `=Banque`\n\n" +
-                ":notes: Concernant l'entrepôt : `=Entrepôt`\n\n" +
-                ":notes: Concernant Les tournois : `=Tournois`\n\n" +
-                ":notes: Concernant la magie : `=Magie`\n\n" +
-                ":notes: Concernant les pauses RP : `=Pause RP`\n\n" +
-                ":notes: Concernant les différentes zones du RP : `=Zones`")
+                ":book: Concernant le contexte : `=Contexte`\n" +
+                ":notes: Concernant la création de personnage : `=Personnage`\n" +       
+                ":notes: Concernant les rolls : `=Roll`\n" +
+                ":notes: Concernant la santé : `=Santé`\n" +
+                ":notes: Concernant la faim : `=Faim`\n" +
+                ":notes: Concernant la soif : `=Soif`\n" +
+                ":notes: Concernant la fatigue : `=Fatigue`\n" +
+                ":notes: Concernant les combats : `=Combat`\n" +
+                ":notes: Concernant la fouille : `=Fouille`\n" +
+                ":notes: Concernant la récolte : `=Récolte`\n" +
+                ":notes: Concernant les différents métiers : `=Métiers`\n" +
+                ":notes: Concernant les taxes en ville : `=Taxes`\n" +
+                ":notes: Concernant la prison : `=Prison`\n" +
+                ":notes: Concernant les trajets : `=Trajets`\n" +
+                ":notes: Concernant les montures : `=Monture`\n" +
+                ":notes: Concernant le système de craft : `=Craft`\n" +
+                ":notes: Concernant la banque : `=Banque`\n" +
+                ":notes: Concernant l'entrepôt : `=Entrepôt`\n" +
+                ":notes: Concernant Les tournois : `=Tournois`\n" +
+                ":notes: Concernant la magie : `=Magie`\n" +
+                ":notes: Concernant les pauses RP : `=Pause RP`\n" +
+                ":notes: Concernant les différentes zones : `=Zones`")
             .setImage("https://images7.alphacoders.com/897/thumb-1920-897952.jpg")
             .setTimestamp()
         message.channel.send({ embed })
