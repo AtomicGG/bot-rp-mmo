8304eb809a37b59e66fb8e04e85815800775eb96
AtomicGG <43680840+AtomicGG@users.noreply.github.com>
2021-03-31 19:44:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,7 +90,7 @@ const exampleEmbed = new Discord.MessageEmbed()
 	.setTimestamp()
 	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
 
-channel.send(exampleEmbed);
+    message.channel.send({ embed })
 }
 });
 
